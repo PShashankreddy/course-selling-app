3d41a790e275adfa6723e83a18dafea37e851190
PShashankreddy <23071a6749@vnrvjiet.in>
2025-10-29 15:43:47 +0530

    ....'

--- index.js
+++ index.js
@@ -12,4 +12,11 @@ app.use("/api/v1/user",UserRouter);
 app.use("/api/v1/course",CourseRouter);
 app.use("/api/v1/admin",AdminRouter);
 
-app.listen(3000);
+async function main()
+{
+  await mongoose.connect("mongodb+srv://23071a6749_db_user:Wan9SXVXYJwQldTj@cluster0.ltbaavk.mongodb.net/coursera-app");
+  app.listen(3000);
+  console.log("Server is running on port 3000");
+}
+
+main();
